75bc69464d85ad3d3f3b1a0781273f58c2149f3a
Влад Давиденко <davidenko.vladislaw2017@yandex.ru>
2024-11-30 18:23:04 +0700

    Второй вариант

---
+++ src/app/data/siteMetadata.js
@@ -0,0 +1,10 @@
+/** @type {import("pliny/config").PlinyConfig } */
+const siteMetadata = {
+  title: 'WARD',
+  author: 'WARD',
+  headerTitle: 'WARD',
+  description: 'WARD web site',
+  language: 'ru',
+}
+
+module.exports = siteMetadata
--- src/app/ui/header/header.tsx
+++ src/app/ui/header/header.tsx
@@ -1,37 +1,46 @@
-import React from "react";
+import siteMetadata from "@/app/data/siteMetadata";
+import headerNavLinks from "@/app/data/nav/headerNavLinks";
 import Link from "next/link";
-import styles from "./header.module.css";
 import Image from "next/image";
 
-const Header: React.FC = () => {
+const Header = () => {
+  let headerClass =
+    "flex items-center w-full bg-white dark:bg-gray-950 justify-between py-10";
+  if (siteMetadata.stickyNav) {
+    headerClass += " sticky top-0 z-50";
+  }
+
   return (
-    <header className={styles.header}>
-      <Image
-        className={styles.image}
-        src="/logo.png"
-        alt="Logo"
-        width={50}
-        height={50}
-      />
-      <div className={styles.logo}>
-        <Link href="/">WARD</Link>
+    <header className={headerClass}>
+      <Link href="/" aria-label={siteMetadata.headerTitle}>
+        <div className="flex items-center justify-between">
+          <div className="mr-3">
+            <Image src="/logo.png" alt="logo" width={40} height={40} />
+          </div>
+          {typeof siteMetadata.headerTitle === "string" ? (
+            <div className="hidden h-6 text-2xl font-semibold sm:block">
+              {siteMetadata.headerTitle}
+            </div>
+          ) : (
+            siteMetadata.headerTitle
+          )}
+        </div>
+      </Link>
+      <div className="flex items-center space-x-4 leading-5 sm:space-x-6">
+        <div className="no-scrollbar hidden max-w-40 items-center space-x-4 overflow-x-auto sm:flex sm:space-x-6 md:max-w-72 lg:max-w-96">
+          {headerNavLinks
+            .filter((link) => link.href !== "/")
+            .map((link) => (
+              <Link
+                key={link.title}
+                href={link.href}
+                className="block font-medium text-gray-900 hover:text-primary-500 dark:text-gray-100 dark:hover:text-primary-400"
+              >
+                {link.title}
+              </Link>
+            ))}
+        </div>
       </div>
-      <nav className={styles.nav}>
-        <ul>
-          <li>
-            <Link href="/home">Главная</Link>
-          </li>
-          <li>
-            <Link href="/home">О нас</Link>
-          </li>
-          <li>
-            <Link href="/home">Услуги</Link>
-          </li>
-          <li>
-            <Link href="/home">Контакты</Link>
-          </li>
-        </ul>
-      </nav>
     </header>
   );
 };
